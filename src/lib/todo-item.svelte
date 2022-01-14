9edea36f44de1a25631c011b5f64175ddf29bbc5
Brenden Ferguson <brenden.ferguson@gmail.com>
2022-01-14 12:32:20 -0800

    apply basic css to todo-item

--- src/lib/todo-item.svelte
+++ src/lib/todo-item.svelte
@@ -1,16 +1,25 @@
-<div>
+<div class="todoBox">
     <form action="" method="">
         <input type="hidden" name="done"/>
-        <button aria-label="Mark done/not done">Done/NotDone</button>
+        <button class="toggle" aria-label="Mark done/not done">Done/NotDone</button>
     </form>
 
     <form action="" method="">
-        <input type="text">
+        <input class="text" type="text">
         <!--  save -->
-        <button aria-label="Save">Save</button>
+        <button class="save" aria-label="Save">Save</button>
     </form>
     <!-- delete -->
     <form action="" method="">
-        <button aria-label="Delete">Delete</button>
+        <button class="delete" aria-label="Delete">Delete</button>
     </form>
 </div>
+
+<style>
+.todoBox{
+    display:flex;
+}
+.todoBox button{
+    margin:1em;
+}
+</style>
